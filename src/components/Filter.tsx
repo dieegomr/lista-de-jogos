910a484a4806aaa16975ed1863d18f4411ddc55a
dieegomr <dmendes.rocha@gmail.com>
2023-06-23 17:47:39 -0300

    feat: pass games as props on Filter component to update filter dinamically

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -1,13 +1,40 @@
 import styles from './Filter.module.css';
 
-export default function Filter() {
+type Game = {
+  id: number;
+  title: string;
+  thumbnail: string;
+  short_description: string;
+  game_url: string;
+  genre: string;
+  platform: string;
+  publisher: string;
+  developer: string;
+  release_date: string;
+  freetogame_profile_url: string;
+};
+
+type FilterProps = {
+  games: Game[];
+};
+
+export default function Filter({ games }: FilterProps) {
+  const genresArray = games
+    ? ['All', ...new Set(games.map((game) => game.genre))]
+    : ['All'];
+
   return (
     <select name="genre" className={styles.genre}>
-      <option value="all">All</option>
+      {/* <option value="all">All</option>
       <option value="shooter">shooter</option>
       <option value="arpg">arpg</option>
       <option value="fighting">fighting</option>
-      <option value="action">Action</option>
+      <option value="action">Action</option> */}
+      {genresArray.map((genre) => (
+        <option value={genre} key={genre}>
+          {genre}
+        </option>
+      ))}
     </select>
   );
 }
