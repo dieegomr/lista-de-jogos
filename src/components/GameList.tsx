4ed599f71ded35fbc633ff897018996ce45b948b
dieegomr <dmendes.rocha@gmail.com>
2023-06-23 14:28:02 -0300

    feat: add error handling depending on API response

--- src/components/GameList.tsx
+++ src/components/GameList.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import GameItem from './GameItem';
 import styles from './GameList.module.css';
 import Loader from './Loader';
+import ErrorMessage from './ErrorMessage';
 
 interface Game {
   id: number;
@@ -20,34 +21,66 @@ interface Game {
 export default function GameList() {
   const [games, setGames] = useState<Game[]>([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState('');
 
   useEffect(function () {
     async function fetchGames() {
-      setIsLoading(true);
-      const res = await fetch(
-        'https://games-test-api-81e9fb0d564a.herokuapp.com/api/data',
-        {
-          method: 'GET',
-          headers: {
-            'dev-email-address': 'test@gmail.com',
-          },
+      try {
+        setIsLoading(true);
+        const res = await fetch(
+          'https://games-test-api-81e9fb0d564a.herokuapp.com/api/data',
+          {
+            method: 'GET',
+            headers: {
+              'dev-email-address': 'test@gmail.com',
+            },
+          }
+        );
+
+        if (
+          res.status === 500 ||
+          res.status === 502 ||
+          res.status === 503 ||
+          res.status === 504 ||
+          res.status === 507 ||
+          res.status === 508 ||
+          res.status === 509
+        )
+          throw new Error(
+            'O servidor falhou em responder, tente recarregar a página'
+          );
+
+        if (!res.ok)
+          throw new Error(
+            'O servidor não conseguirá responder por agora, tente voltar novamente mais tarde'
+          );
+
+        const data = await res.json();
+        console.log(data);
+        setGames(data);
+        setIsLoading(false);
+      } catch (err: unknown) {
+        if (err instanceof Error) {
+          setError(err.message);
+        } else {
+          setError(
+            'O servidor não conseguirá responder por agora, tente voltar novamente mais tarde'
+          );
         }
-      );
-      const data = await res.json();
-      console.log(data);
-      setGames(data);
-      setIsLoading(false);
+      } finally {
+        setIsLoading(false);
+      }
     }
     fetchGames();
   }, []);
 
   return (
     <ul className={styles.list}>
-      {isLoading ? (
-        <Loader />
-      ) : (
-        games.map((game) => <GameItem game={game} key={game.id} />)
-      )}
+      {isLoading && !error && <Loader />}
+      {!isLoading &&
+        !error &&
+        games.map((game) => <GameItem game={game} key={game.id} />)}
+      {error && <ErrorMessage message={error} />}
     </ul>
   );
 }
