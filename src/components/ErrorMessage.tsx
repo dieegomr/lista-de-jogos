d8625d776f6533fcb80c514e47b0143ef55b77f6
dieegomr <dmendes.rocha@gmail.com>
2023-06-25 07:52:12 -0300

    style: style error message

--- src/components/ErrorMessage.tsx
+++ src/components/ErrorMessage.tsx
@@ -2,5 +2,9 @@ import { ErrorMessageProps } from '../types/ErrorMessageProps';
 import styles from './ErrorMessage.module.css';
 
 export default function ErrorMessage({ message }: ErrorMessageProps) {
-  return <p className={styles.error}>{message}</p>;
+  return (
+    <div className={styles.error}>
+      <p className={styles.message}>{message}</p>
+    </div>
+  );
 }
