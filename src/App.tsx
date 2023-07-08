22363335f3b360402a3a09d11cd4d477d1792d80
dieegomr <dmendes.rocha@gmail.com>
2023-07-08 14:18:50 -0300

    chore: install react-router-dom and configure basic routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,16 @@
-import { useState } from 'react';
-
-import GameList from './components/GameList';
-import Logo from './components/Logo';
-import NavBar from './components/NavBar';
-import Search from './components/Search';
-import Layout from './ui/Layout';
-import { useGames } from './hooks/useGames';
-import Main from './components/Main';
-import ErrorMessage from './components/ErrorMessage';
-import GenreSelector from './components/GenreSelector';
-import Loader from './ui/Loader';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import AuthenticationPage from './pages/AuthenticationPage';
 
 export default function App() {
-  const [searchQuery, setSearchQuery] = useState('');
-  const [selectedGenre, setSelectedGenre] = useState('All');
-
-  const { games, isLoading, error } = useGames();
-
   return (
-    <Layout>
-      <NavBar>
-        <Logo />
-        <Search searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
-      </NavBar>
-      <section>
-        <GenreSelector
-          games={games}
-          selectedGenre={selectedGenre}
-          setSelectedGenre={setSelectedGenre}
-        />
-      </section>
-      <Main>
-        {isLoading && !error && <Loader />}
-        {error && !isLoading && <ErrorMessage message={error} />}
-        {!isLoading && !error && (
-          <GameList
-            searchQuery={searchQuery}
-            selectedGenre={selectedGenre}
-            games={games}
-          />
-        )}
-      </Main>
-    </Layout>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Navigate replace to="games" />} />
+        <Route path="auth" element={<AuthenticationPage />} />
+        <Route path="games" element={<p>games</p>} />
+        <Route path="favorites" element={<p>favorites</p>} />
+        <Route></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
