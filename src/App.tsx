2233110463b114bcff29e022dbd0fac1ea56e9a2
dieegomr <dmendes.rocha@gmail.com>
2023-07-08 14:41:30 -0300

    feat: add MainPage page

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,42 @@
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import AuthenticationPage from './pages/AuthenticationPage';
+import MainPage from './pages/MainPage';
+import GameList from './components/GameList';
+import { useState } from 'react';
+import { useGames } from './hooks/useGames';
 
 export default function App() {
+  const [searchQuery, setSearchQuery] = useState('');
+  const [selectedGenre, setSelectedGenre] = useState('All');
+  const { games } = useGames();
+
   return (
     <BrowserRouter>
       <Routes>
         <Route index element={<Navigate replace to="games" />} />
         <Route path="auth" element={<AuthenticationPage />} />
-        <Route path="games" element={<p>games</p>} />
-        <Route path="favorites" element={<p>favorites</p>} />
+        <Route
+          element={
+            <MainPage
+              searchQuery={searchQuery}
+              setSearchQuery={setSearchQuery}
+              selectedGenre={selectedGenre}
+              setSelectedGenre={setSelectedGenre}
+            />
+          }
+        >
+          <Route
+            path="games"
+            element={
+              <GameList
+                games={games}
+                searchQuery={searchQuery}
+                selectedGenre={selectedGenre}
+              />
+            }
+          />
+          <Route path="favorites" element={<p>favorites</p>} />
+        </Route>
         <Route></Route>
       </Routes>
     </BrowserRouter>
---
+++ src/pages/MainPage.tsx
@@ -0,0 +1,41 @@
+import { Outlet } from 'react-router-dom';
+import GenreSelector from '../components/GenreSelector';
+import Logo from '../components/Logo';
+import Main from '../components/Main';
+import NavBar from '../components/NavBar';
+import Search from '../components/Search';
+import styles from './MainPage.module.css';
+import { useGames } from '../hooks/useGames';
+
+interface MainPageProps {
+  searchQuery: string;
+  setSearchQuery: React.Dispatch<React.SetStateAction<string>>;
+  selectedGenre: string;
+  setSelectedGenre: React.Dispatch<React.SetStateAction<string>>;
+}
+
+export default function MainPage({
+  searchQuery,
+  setSearchQuery,
+  selectedGenre,
+  setSelectedGenre,
+}: MainPageProps) {
+  const { games } = useGames();
+
+  return (
+    <div className={styles.app}>
+      <NavBar>
+        <Logo />
+        <Search searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
+      </NavBar>
+      <GenreSelector
+        games={games}
+        selectedGenre={selectedGenre}
+        setSelectedGenre={setSelectedGenre}
+      />
+      <Main>
+        <Outlet />
+      </Main>
+    </div>
+  );
+}
