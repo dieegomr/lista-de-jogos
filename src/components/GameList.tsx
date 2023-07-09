9010136bd6dae9841ac01da80382979d672730f9
dieegomr <dmendes.rocha@gmail.com>
2023-07-09 08:24:36 -0300

    fix: fix bug that was preventing error message, loader and GameList from being rendered conditionally

--- src/components/GameList.tsx
+++ src/components/GameList.tsx
@@ -5,23 +5,30 @@ import { GameListProps } from '../types/GameListProps';
 import { filterGamesByGenre, searchGamesByQuery } from '../utils/helpers';
 import { useGames } from '../contexts/gameContext/hook';
 import { useSearchQuery } from '../contexts/searchContext/hook';
+import Loader from '../ui/Loader';
 
 export default function GameList({ selectedGenre }: GameListProps) {
-  const { games } = useGames();
+  const { games, isLoading, error } = useGames();
   const { searchQuery } = useSearchQuery();
 
   const filteredGames = filterGamesByGenre(games, selectedGenre);
 
   const searchedGames = searchGamesByQuery(filteredGames, searchQuery);
 
-  if (!searchedGames.length)
+  if (!isLoading && !error && !searchedGames.length)
     return <ErrorMessage message="Nenhum jogo encontrado 😕" />;
 
   return (
-    <ul className={styles.list}>
-      {searchedGames.map((game) => (
-        <GameItem game={game} key={game.id} />
-      ))}
-    </ul>
+    <>
+      {isLoading && !error && <Loader />}
+      {error && !isLoading && <ErrorMessage message={error} />}
+      {!isLoading && !error && (
+        <ul className={styles.list}>
+          {searchedGames.map((game) => (
+            <GameItem game={game} key={game.id} />
+          ))}
+        </ul>
+      )}
+    </>
   );
 }
