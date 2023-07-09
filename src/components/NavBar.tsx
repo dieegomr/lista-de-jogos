853497afa715a627fb66417562b581c87fbc5b3c
dieegomr <dmendes.rocha@gmail.com>
2023-07-09 13:16:10 -0300

    refactor: move mainPage logic to where it belongs (NavBar)

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,6 +1,38 @@
-import { NavBarProps } from '../types/NavBarProps';
 import styles from './NavBar.module.css';
 
-export default function NavBar({ children }: NavBarProps) {
-  return <nav className={styles.navBar}>{children}</nav>;
+import Logo from '../components/Logo';
+import Search from '../components/Search';
+import ActionButton from '../components/ActionButton';
+import { useAuth } from '../contexts/authContext/hook';
+import { useNavigate } from 'react-router-dom';
+
+export default function NavBar() {
+  const { logout, isAuthenticated } = useAuth();
+  const navigate = useNavigate();
+
+  function handleAuth() {
+    if (isAuthenticated) {
+      logout();
+      navigate('/');
+    } else {
+      navigate('/auth');
+    }
+  }
+
+  function handleFavorites() {
+    isAuthenticated
+      ? navigate('/favorites')
+      : alert('é necessario logar para acessar os favoritos');
+  }
+
+  return (
+    <nav className={styles.navBar}>
+      <Logo />
+      <Search />
+      <ActionButton onClick={handleAuth}>
+        {isAuthenticated ? 'Logout' : 'Login'}
+      </ActionButton>
+      <ActionButton onClick={handleFavorites}>Favorites</ActionButton>
+    </nav>
+  );
 }
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,42 +1,14 @@
-import { Outlet, useNavigate } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import GenreSelector from '../components/GenreSelector';
-import Logo from '../components/Logo';
 import Main from '../components/Main';
 import NavBar from '../components/NavBar';
-import Search from '../components/Search';
+
 import styles from './MainPage.module.css';
-import ActionButton from '../components/ActionButton';
-import { useAuth } from '../contexts/authContext/hook';
 
 export default function MainPage() {
-  const { logout, isAuthenticated } = useAuth();
-  const navigate = useNavigate();
-
-  function handleAuth() {
-    if (isAuthenticated) {
-      logout();
-      navigate('/');
-    } else {
-      navigate('/auth');
-    }
-  }
-
-  function handleFavorites() {
-    isAuthenticated
-      ? navigate('/favorites')
-      : alert('é necessario logar para acessar os favoritos');
-  }
-
   return (
     <div className={styles.app}>
-      <NavBar>
-        <Logo />
-        <Search />
-        <ActionButton onClick={handleAuth}>
-          {isAuthenticated ? 'Logout' : 'Login'}
-        </ActionButton>
-        <ActionButton onClick={handleFavorites}>Favorites</ActionButton>
-      </NavBar>
+      <NavBar />
       <GenreSelector />
       <Main>
         <Outlet />
