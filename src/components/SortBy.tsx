b27ce36910dea15264a8031a825b696aa716adf7
dieegomr <dmendes.rocha@gmail.com>
2023-07-10 11:44:55 -0300

    fix: ensure sortBy button render when logged in even when there is no sort selected

--- src/components/SortBy.tsx
+++ src/components/SortBy.tsx
@@ -1,6 +1,8 @@
 import { useSearchParams } from 'react-router-dom';
+import { useAuth } from '../contexts/authContext/hook';
 
 export default function SortBy() {
+  const { isAuthenticated } = useAuth();
   const [searchParams, setSearchParams] = useSearchParams();
   const sortBy = searchParams.get('sortBy');
 
@@ -16,10 +18,13 @@ export default function SortBy() {
 
   return (
     <>
-      {sortBy === 'rating-desc' && (
+      {!sortBy && isAuthenticated && (
         <button onClick={handleSortAsc}>Sort by rating &#8593;</button>
       )}
-      {sortBy === 'rating-asc' && (
+      {sortBy === 'rating-desc' && isAuthenticated && (
+        <button onClick={handleSortAsc}>Sort by rating &#8593;</button>
+      )}
+      {sortBy === 'rating-asc' && isAuthenticated && (
         <button onClick={handleSortDesc}>Sort by rating &#8595;</button>
       )}
     </>
