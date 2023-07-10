99f820fb7ffc6a289e09942cdfb040dec63a6920
dieegomr <dmendes.rocha@gmail.com>
2023-07-10 11:39:44 -0300

    feat: only show rated and favorited games when logged in

--- src/components/FavoriteGameIcon.tsx
+++ src/components/FavoriteGameIcon.tsx
@@ -3,22 +3,32 @@ import styles from './HeartIcon.module.css';
 import { Game } from '../types/Game';
 import HeartIcon from './HeartIcon';
 import { useFavorites } from '../contexts/favoritesContext/hook';
+import { useAuth } from '../contexts/authContext/hook';
+import { useNavigate } from 'react-router-dom';
 
 interface FavoriteGameIconProps {
   game: Game;
 }
 
 export default function FavoriteGameIcon({ game }: FavoriteGameIconProps) {
+  const navigate = useNavigate();
+
   const [tempFull, setTempFull] = useState(false);
 
   const { favorites, addFavoriteGame, removeFavoriteGame } = useFavorites();
+  const { isAuthenticated } = useAuth();
 
   const isFavorite = favorites
     .map((favoriteGame) => favoriteGame.id)
     .includes(game.id);
 
   async function handleClick(game: Game) {
-    isFavorite ? removeFavoriteGame(game.id) : addFavoriteGame(game);
+    if (!isAuthenticated) {
+      alert('voce precisa estar logado');
+      navigate('/auth');
+    } else {
+      isFavorite ? removeFavoriteGame(game.id) : addFavoriteGame(game);
+    }
   }
 
   return (
@@ -28,7 +38,10 @@ export default function FavoriteGameIcon({ game }: FavoriteGameIconProps) {
       onMouseLeave={() => setTempFull(false)}
       onClick={() => handleClick(game)}
     >
-      <HeartIcon full={isFavorite ? isFavorite : tempFull} />
+      {isAuthenticated && (
+        <HeartIcon full={isFavorite ? isFavorite : tempFull} />
+      )}
+      {!isAuthenticated && <HeartIcon full={tempFull} />}
     </span>
   );
 }
--- src/components/StarRating.tsx
+++ src/components/StarRating.tsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import Star from './Star';
 import { useRatedGames } from '../contexts/ratedGamesContext/hook';
+import { useAuth } from '../contexts/authContext/hook';
+import { useNavigate } from 'react-router-dom';
 
 const containerStyle = {
   display: 'flex',
@@ -18,29 +20,47 @@ interface StarRatingProps {
 }
 
 export default function StarRating({ maxRating = 4, gameId }: StarRatingProps) {
+  const { isAuthenticated } = useAuth();
+  const navigate = useNavigate();
   const { addRatedGame, getRate } = useRatedGames();
   const rate = getRate(gameId);
   const [rating, setRating] = useState(rate);
   const [tempRating, setTempRating] = useState(0);
 
   function handleRating(rating: number) {
-    const ratedGame = { gameId: gameId, rate: rating };
-    addRatedGame(ratedGame);
-    setRating(rating);
+    if (!isAuthenticated) {
+      alert('voce precisa estar logado');
+      navigate('/auth');
+    } else {
+      const ratedGame = { gameId: gameId, rate: rating };
+      addRatedGame(ratedGame);
+      setRating(rating);
+    }
   }
 
   return (
     <div style={containerStyle}>
       <div style={starContainerStyle}>
-        {Array.from({ length: maxRating }, (_, i) => (
-          <Star
-            key={i}
-            full={tempRating ? tempRating >= i + 1 : rating >= i + 1}
-            onRate={() => handleRating(i + 1)}
-            onHoverIn={() => setTempRating(i + 1)}
-            onHoverOut={() => setTempRating(0)}
-          />
-        ))}
+        {isAuthenticated &&
+          Array.from({ length: maxRating }, (_, i) => (
+            <Star
+              key={i}
+              full={tempRating ? tempRating >= i + 1 : rating >= i + 1}
+              onRate={() => handleRating(i + 1)}
+              onHoverIn={() => setTempRating(i + 1)}
+              onHoverOut={() => setTempRating(0)}
+            />
+          ))}
+        {!isAuthenticated &&
+          Array.from({ length: maxRating }, (_, i) => (
+            <Star
+              key={i}
+              full={tempRating >= i + 1}
+              onRate={() => handleRating(i + 1)}
+              onHoverIn={() => setTempRating(i + 1)}
+              onHoverOut={() => setTempRating(0)}
+            />
+          ))}
       </div>
     </div>
   );
